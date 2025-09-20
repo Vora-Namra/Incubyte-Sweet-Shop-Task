48901008c081a7d55b80bbb2f20d48fa2f5d41ba
unknown <voranamra625@gmail.com>
2025-09-20 20:05:02 +0530

    adding ss

--- frontend/src/api/auth.tsx
+++ frontend/src/api/auth.tsx
@@ -1,5 +1,5 @@
-const API_URL = (import.meta.env.VITE_BACKEND_URL ?? "https://incubyte-sweet-shop-task-d2jr.vercel.app") + "/api/auth";
-// const API_URL = "http://localhost:5000/api/auth";
+// const API_URL = (import.meta.env.VITE_BACKEND_URL ?? "https://incubyte-sweet-shop-task-d2jr.vercel.app") + "/api/auth";
+const API_URL = "http://localhost:5000/api/auth";
 export async function registerUser(data: { name: string; email: string; password: string }) {
   const res = await fetch(`${API_URL}/register`, {
     method: "POST",
